53c99464daf07fa8bbe357d8c029458da9f5d6b3
Seulchan <seulchan.lee@pm.me>
2026-01-10 04:12:52 +0900

    chore: add annotation to SocialCredentialRepository

--- src/main/kotlin/com/wafflestudio/team8server/user/repository/SocialCredentialRepository.kt
+++ src/main/kotlin/com/wafflestudio/team8server/user/repository/SocialCredentialRepository.kt
@@ -4,6 +4,7 @@ import com.wafflestudio.team8server.user.model.SocialCredential
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
+@Repository
 interface SocialCredentialRepository : JpaRepository<SocialCredential, Long> {
     fun findByProviderIdAndSocialId(provider: String, socialId: String): SocialCredential?
     fun existsByProviderIdAndSocialId(provider: String, socialId: String): Boolean
