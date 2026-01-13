e6d55551d7a257a4eb23e11eb50862a6cb5eac19
Seulchan <seulchan.lee@pm.me>
2026-01-14 02:00:43 +0900

    feat: implement pre-enroll api with swagger docs

---
+++ src/main/kotlin/com/wafflestudio/team8server/preenroll/dto/PreEnrollCourseResponse.kt
@@ -0,0 +1,14 @@
+package com.wafflestudio.team8server.preenroll.dto
+
+import com.wafflestudio.team8server.course.dto.CourseDetailResponse
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(description = "장바구니 조회 응답")
+data class PreEnrollCourseResponse(
+    @Schema(description = "장바구니 ID")
+    val preEnrollId: Long,
+    @Schema(description = "강의 정보")
+    val course: CourseDetailResponse,
+    @Schema(description = "담은 사람 수")
+    val cartCount: Int,
+)
