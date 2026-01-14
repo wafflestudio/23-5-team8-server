4b271bed2b4cca2869363862541b150d47368978
Seulchan <seulchan.lee@pm.me>
2026-01-14 16:58:45 +0900

    docs: clarity description for pre-enroll response

--- src/main/kotlin/com/wafflestudio/team8server/preenroll/dto/PreEnrollCourseResponse.kt
+++ src/main/kotlin/com/wafflestudio/team8server/preenroll/dto/PreEnrollCourseResponse.kt
@@ -3,7 +3,7 @@ package com.wafflestudio.team8server.preenroll.dto
 import com.wafflestudio.team8server.course.dto.CourseDetailResponse
 import io.swagger.v3.oas.annotations.media.Schema
 
-@Schema(description = "장바구니 조회 응답")
+@Schema(description = "장바구니의 특정 강의 정보")
 data class PreEnrollCourseResponse(
     @Schema(description = "장바구니 ID")
     val preEnrollId: Long,
