ebe6208d2cb673429c81294eb6be0f187b1efa9a
Seulchan <seulchan.lee@pm.me>
2026-01-27 12:43:57 +0900

    fix: bulk delete when import courses

--- src/main/kotlin/com/wafflestudio/team8server/course/repository/CourseRepository.kt
+++ src/main/kotlin/com/wafflestudio/team8server/course/repository/CourseRepository.kt
@@ -4,10 +4,20 @@ import com.wafflestudio.team8server.course.model.Course
 import com.wafflestudio.team8server.course.model.Semester
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 
 interface CourseRepository :
     JpaRepository<Course, Long>,
     JpaSpecificationExecutor<Course> {
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Query(
+        """
+        delete from Course c
+        where c.year = :year
+          and c.semester = :semester
+        """,
+    )
     fun deleteAllByYearAndSemester(
         year: Int,
         semester: Semester,
